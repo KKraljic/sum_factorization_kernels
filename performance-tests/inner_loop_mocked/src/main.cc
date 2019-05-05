af7746e55965624bb5ef4cf105ef734666394f58
Karlo Kraljic <ga27rus2@mpp2-login5.cos.lrz.de>
2019-05-05 21:41:11 +0200

    Add measurements to code

--- performance-tests/inner_loop_mocked/src/main.cc
+++ performance-tests/inner_loop_mocked/src/main.cc
@@ -16,6 +16,7 @@ const unsigned int max_degree = 12;
 const unsigned int dimension = 3;
 typedef double value_type;
 char region_tag[100];
+int max_num_threads = 28;
 
 
 /// \brief Calculates the total number of cells.
@@ -46,6 +47,7 @@ void init_number_of_cells_per_dim(unsigned int output_arr[], const unsigned int 
     }
 
 }
+
 template<int dim, int degree, typename Number>
 void set_block_sizes(EvaluationDGLaplacian<dim, degree, Number> &evaluator){
     evaluator.blx = std::max(20 / (degree + 1), 3);
@@ -62,48 +64,57 @@ void adjust_total_number_of_cells_to_block_sizes(unsigned int in_n_cells[], unsi
                  VectorizedArray<Number>::n_array_elements;
 }
 
-
-
 template<int dim, int degree, typename Number>
 void run(const unsigned int n_tests) {
         if (degree < 3 || degree > 12){
             std::cout << "Only degree 3-12 implemented, aborting."<< std::endl;
             return;
         }
-        unsigned long long vector_size = 100000000;
-        EvaluationDGLaplacian<dim, degree, Number> evaluator;
-        const unsigned int n_cells_tot= get_total_number_of_cells<dim, degree>(vector_size);
-        unsigned int initial_n_cells[3];
-        unsigned int adjusted_n_cells[3];
+        for(unsigned long long vector_size = 1; vector_size < pow(2, 63)-1; vector_size*=2){
+            for(int num_threads = 1; num_threads <= max_num_threads; num_threads++) {
+                omp_set_num_threads(num_threads);
+                #pragma omp parallel
+                {
+                    LIKWID_MARKER_THREADINIT;
+                }
 
+                EvaluationDGLaplacian<dim, degree, Number> evaluator;
+                const unsigned int n_cells_tot = get_total_number_of_cells<dim, degree>(vector_size);
+                unsigned int initial_n_cells[3];
+                unsigned int adjusted_n_cells[3];
 
-        init_number_of_cells_per_dim<dim, Number>(initial_n_cells, n_cells_tot);
-        set_block_sizes<dim, degree, Number>(evaluator);
-        adjust_total_number_of_cells_to_block_sizes<dim, degree, Number>(initial_n_cells, adjusted_n_cells, evaluator, n_cells_tot);
+                init_number_of_cells_per_dim<dim, Number>(initial_n_cells, n_cells_tot);
+                set_block_sizes<dim, degree, Number>(evaluator);
+                adjust_total_number_of_cells_to_block_sizes<dim, degree, Number>(initial_n_cells, adjusted_n_cells,
+                                                                                 evaluator, n_cells_tot);
 
-        evaluator.initialize(adjusted_n_cells);
+                evaluator.initialize(adjusted_n_cells);
 
-        for (unsigned int ib = 0; ib < evaluator.n_blocks[2]; ++ib)
-            for (unsigned int jb = 0; jb < evaluator.n_blocks[1]; ++jb)
-                for (unsigned int kb = 0; kb < evaluator.n_blocks[0]; ++kb)
-                    for (unsigned int i = ib * evaluator.blz; i < std::min(evaluator.n_cells[2], (ib + 1) * evaluator.blz); ++i)
-                        for (unsigned int j = jb * evaluator.bly; j < std::min(evaluator.n_cells[1], (jb + 1) * evaluator.bly); ++j) {
-                            evaluator.do_inner_loop
-                                    (kb * evaluator.blx, std::min(evaluator.n_cells[0], (kb + 1) * evaluator.blx), j, i,
-                                     evaluator.sol_new, evaluator.sol_new, evaluator.sol_new, evaluator.sol_new,
-                                     evaluator.sol_tmp,
-                                     0., 0.);
-                        }
+                sprintf(region_tag, "Inner-Loop-t-%i-n-%u", omp_get_num_threads(), n_cells_tot);
+                LIKWID_MARKER_START(region_tag);
+                #pragma omp parallel for schedule (static) collapse(2)
+                for (unsigned int ib = 0; ib < evaluator.n_blocks[2]; ++ib)
+                    for (unsigned int jb = 0; jb < evaluator.n_blocks[1]; ++jb)
+                        for (unsigned int kb = 0; kb < evaluator.n_blocks[0]; ++kb)
+                            for (unsigned int i = ib * evaluator.blz;
+                                 i < std::min(evaluator.n_cells[2], (ib + 1) * evaluator.blz); ++i)
+                                for (unsigned int j = jb * evaluator.bly;
+                                     j < std::min(evaluator.n_cells[1], (jb + 1) * evaluator.bly); ++j) {
+                                    evaluator.do_inner_loop
+                                            (kb * evaluator.blx,
+                                             std::min(evaluator.n_cells[0], (kb + 1) * evaluator.blx), j, i,
+                                             evaluator.sol_new, evaluator.sol_new, evaluator.sol_new, evaluator.sol_new,
+                                             evaluator.sol_tmp,
+                                             0., 0.);
+                                }
+                LIKWID_MARKER_STOP(region_tag);
+            }
+        }
     }
 
 int main(int argc, char **argv) {
 
-    //LIKWID_MARKER_INIT;
-/*#pragma omp parallel
-    {
-      LIKWID_MARKER_THREADINIT;
-    }
-#endif*/
+    LIKWID_MARKER_INIT;
     unsigned long long int vector_size, n_tests;
     std::string::size_type sz = 0;
 
@@ -114,10 +125,10 @@ int main(int argc, char **argv) {
 
     n_tests = 10;
 
-    run<3, 4, double>(n_tests);
+    run<2, 4, double>(n_tests);
 
 
-    //LIKWID_MARKER_CLOSE;
+    LIKWID_MARKER_CLOSE;
 
     return 0;
 }
